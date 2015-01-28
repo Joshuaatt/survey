04168799e6332bc1f8d1dd699caf4aa29ce41d7d
Luke Chinworth & Joshua Atteberry <student@epicodus.com>
2015-01-28 10:43:15 -0800

    add questions table

--- app.rb
+++ app.rb
@@ -1,3 +1,24 @@
 require 'bundler/setup'
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+get '/' do
+  @surveys = Survey.all
+  erb :index
+end
+
+post '/' do
+  survey = Survey.create({ name: params.fetch('name') })
+  @surveys = Survey.all
+  erb :index
+end
+
+get '/surveys/:id' do
+  @survey = Survey.find(params.fetch("id").to_i)
+  erb :survey
+end
+
+post '/surveys/:id' do
+
+end
